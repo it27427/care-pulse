919154ea365dbb7db34ac4a9e8dbdf7904bdc20d
it27427 <istiak.tushar.qtec@gmail.com>
2024-07-10 11:42:26 +0600

    favicon updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,12 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
+import { cn } from "@/lib/utils";
+
 const fontSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
@@ -19,7 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={fontSans.className} suppressHydrationWarning={true}>
+      <body
+        className={cn("min-h-screen bg-dark-300 font-sans")}
+        suppressHydrationWarning={true}
+      >
         {children}
       </body>
     </html>
