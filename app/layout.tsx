6f67648d2858d31dc4fc52c97e51dfb7aaff562f
it27427 <istiak.tushar.qtec@gmail.com>
2024-07-10 11:32:45 +0600

    font changed from inter to Plus_Jakarta_Sans

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
 
 export const metadata: Metadata = {
   title: "CarePulse",
@@ -16,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className} suppressHydrationWarning={true}>
+      <body className={fontSans.className} suppressHydrationWarning={true}>
         {children}
       </body>
     </html>
