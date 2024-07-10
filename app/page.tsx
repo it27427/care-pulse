b4398d0cc8140d1c164920598e9551552aefe7c2
it27427 <istiak.tushar.qtec@gmail.com>
2024-07-10 15:54:39 +0600

    logo added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
-import React from "react";
-import { Button } from "@/components/ui/button";
+import React from 'react';
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 
 const page = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <Button>Click me</Button>
+    <div className='flex h-screen max-h-screen'>
+      <section className='remove-scrollbar container my-auto'>
+        <div className='sub-container max-w-[496px]'>
+          <Image
+            src='/assets/icons/logo-full.svg'
+            width={1000}
+            height={1000}
+            className='mb-12 h-10 w-fit'
+            alt='BRAND-LOGO'
+            priority
+          />
+        </div>
+      </section>
     </div>
   );
 };
