47b15a1082d2de00248899a75d9119fffa06993f
it27427 <istiak.tushar.qtec@gmail.com>
2024-07-11 10:44:01 +0600

    formSchema Updated

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,13 +1,32 @@
 'use client';
 
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 
 const formSchema = z.object({
-  username: z.string().min(2).max(50),
+  username: z.string().min(3, {
+    message: 'Username must be at least 3 characters.',
+  }),
 });
 
 const PatientForm = () => {
-  return <div>Patient Form</div>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <div>
+      <h5 className='m-0'>Patient Form</h5>
+    </div>
+  );
 };
 
 export default PatientForm;
