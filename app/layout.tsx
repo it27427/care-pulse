042a72d3eb67f77f9081400ee4c06c9f963b2c5d
it27427 <istiak.tushar.qtec@gmail.com>
2024-07-28 11:25:34 +0600

    ColorTheme Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ const RootLayout = ({
   children: React.ReactNode;
 }>) => {
   return (
-    <html lang='en' className='dark' style={{ colorScheme: 'dark' }}>
+    <html lang='en' className='dark'>
       <body
         className={cn(
           'min-h-screen bg-dark-300 font-sans antialiased',
